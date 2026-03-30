085f91d61d9b2db1c00959b21039740ab3ae744a
lelongilbert7-gif <lelongilbert7@gmail.com>
2026-03-30 20:32:01 +0300

    Update 07_data_validation.sql

--- sql/07_data_validation.sql
+++ sql/07_data_validation.sql
@@ -1,1 +1,22 @@
-data validation sql
+SELECT 'stg_customers' AS table_name, COUNT(*) AS row_count
+FROM `bluepeak-commerce-analytics.BluePeak_Staging.stg_customers`
+
+UNION ALL
+SELECT 'stg_products', COUNT(*)
+FROM `bluepeak-commerce-analytics.BluePeak_Staging.stg_products`
+
+UNION ALL
+SELECT 'stg_orders', COUNT(*)
+FROM `bluepeak-commerce-analytics.BluePeak_Staging.stg_orders`
+
+UNION ALL
+SELECT 'stg_order_items', COUNT(*)
+FROM `bluepeak-commerce-analytics.BluePeak_Staging.stg_order_items`
+
+UNION ALL
+SELECT 'stg_refunds', COUNT(*)
+FROM `bluepeak-commerce-analytics.BluePeak_Staging.stg_refunds`
+
+UNION ALL
+SELECT 'stg_transactions', COUNT(*)
+FROM `bluepeak-commerce-analytics.BluePeak_Staging.stg_transactions`;

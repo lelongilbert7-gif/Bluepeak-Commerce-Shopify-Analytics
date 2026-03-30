d1a22978f01f2289e7c0b0a6d46e072f6f250038
lelongilbert7-gif <lelongilbert7@gmail.com>
2026-03-30 20:45:04 +0300

    Update 09_validation for the fact table.sql

--- sql/09_validation for the fact table.sql
+++ sql/09_validation for the fact table.sql
@@ -1,1 +1,3 @@
-validation for the fact table
+SELECT
+    (SELECT COUNT(*) FROM `bluepeak-commerce-analytics.BluePeak_Staging.stg_order_items`) AS stg_order_items_count,
+    (SELECT COUNT(*) FROM `bluepeak-commerce-analytics.BluePeak_Staging.fact_profitability`) AS fact_profitability_count;
